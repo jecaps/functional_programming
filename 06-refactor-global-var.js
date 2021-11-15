a1f9e933883996b9e8c5eabca622826145b39557
Jerika Kanz <jerikacapila@gmail.com>
2021-11-15 20:57:26 +0100

    06 Refactor Global Variables Out of Functions

---
+++ 06-refactor-global-var.js
@@ -0,0 +1,36 @@
+/*
+So far, we have seen two distinct principles for functional programming:
+
+Don't alter a variable or object - create new variables and objects and return them if need be from a function. Hint: using something like const newArr = arrVar, where arrVar is an array will simply create a reference to the existing variable and not a copy. So changing a value in newArr would change the value in arrVar.
+
+Declare function parameters - any computation inside a function depends only on the arguments passed to the function, and not on any global object or variable.
+
+Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.
+*/
+
+// Challenge
+
+const bookList = [
+  "The Hound of the Baskervilles",
+  "On The Electrodynamics of Moving Bodies",
+  "Philosophiæ Naturalis Principia Mathematica",
+  "Disquisitiones Arithmeticae",
+];
+
+function add(list, bookName) {
+  return [...list, bookName];
+}
+function remove(list, bookName) {
+  return list.filter((book) => book === bookName);
+}
+const newBookList = add(bookList, "A Brief History of Time");
+const newerBookList = remove(
+  bookList,
+  "On The Electrodynamics of Moving Bodies"
+);
+const newestBookList = remove(
+  add(bookList, "A Brief History of Time"),
+  "On The Electrodynamics of Moving Bodies"
+);
+
+console.log(newBookList, newerBookList, newestBookList);
